56fd14c775cfe0d04d3ef14d573da257d95b94e6
김시영 <siyoung@woowahan.com>
2020-12-09 17:06:38 +0900

    [STPILOT-92] 파일럿 프로젝트 생성

---
+++ src/main/java/in/woowa/settle/pilotproject/PilotProjectApplication.java
@@ -0,0 +1,13 @@
+package in.woowa.settle.pilotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilotProjectApplication.class, args);
+    }
+
+}
